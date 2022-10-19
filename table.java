38cc5e7dfa23090c18b6e698cf391ba5dfa6aa7d
Kishu <kishu450kk@gmail.com>
2022-10-19 22:11:17 +0530

    program for print the table

---
+++ table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        
+        for (int i=1; i<=10; i++){
+            System.out.println(n+"*"+i+"="+(n*i));
+        }
+       
+    }
+}
