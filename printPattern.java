0995411db6cd020729fd07db2f71317476339beb
Kishu <kishu450kk@gmail.com>
2022-10-19 12:14:10 +0530

    first pattern program

---
+++ printPattern.java
@@ -0,0 +1,8 @@
+public class printPattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
