eccca261451d4be481e9ed6e9d6151e68a8713d0
Kishu <kishu450kk@gmail.com>
2022-10-19 20:21:14 +0530

    calculator program using switch

---
+++ calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Select operation: ");
+        int operator = sc.nextInt();
+
+        switch (operator){
+            case 1 : System.out.println("Sum of "+a+" and "+b+" is "+(a+b));
+            break;
+            case 2 : System.out.println("Sub of "+a+" and "+b+" is "+(a-b));
+            break;
+            case 3 : System.out.println("Mul of "+a+" and "+b+" is "+(a*b));
+            break;
+            case 4 : System.out.println("Div of "+a+" and "+b+" is "+(a/b));
+            break;
+            default : System.out.println("Invalid");
+
+        }
+    }
+}
