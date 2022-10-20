d07101397525a08d9c64692551e1f780decb5351
Kishu <kishu450kk@gmail.com>
2022-10-20 23:38:32 +0530

    program for space half pyramid

---
+++ space_half_pyramid.java
@@ -0,0 +1,15 @@
+public class space_half_pyramid {
+    public static void main(String[] args) {
+        int a= 4;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a-i; j++) {
+                System.out.print(" "); 
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        
+        }
+    }
+}
