f0f313643f642356d08df3bd43576e6f7d0080b4
kylix-p <priyanshu450ss@gmail.com>
2022-10-21 01:07:00 +0530

    Added some rules for row and column

--- hollow_rectangle.java
+++ hollow_rectangle.java
@@ -3,7 +3,9 @@ public class hollow_rectangle {
         int a=4;
         int b=5;
         
+        // Outer loop for Rows
         for (int i=1; i<=a; i++){
+            // Inner loop Column
             for (int j = 1; j <= b; j++) {
                 if (i==1||j==1||i==a||j==b) {
                   System.out.print("*");  
