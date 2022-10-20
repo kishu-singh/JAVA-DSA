439e872a5eee52940ac519f485fdb36e7beae574
Kishu <kishu450kk@gmail.com>
2022-10-20 23:10:35 +0530

    prohram for inverted half pyramid

---
+++ inverted_half_pyramid.java
@@ -0,0 +1,11 @@
+public class inverted_half_pyramid {
+    public static void main(String[] args) {
+        int a=4;
+        for (int i = a; i>=1; i--) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
