82de8703d084b7deb981b2a477b1e0ad024741b3
Kishu <kishu450kk@gmail.com>
2022-10-20 20:11:02 +0530

    program for solid rectangle

---
+++ solid_rectangle.java
@@ -0,0 +1,12 @@
+public class solid_rectangle {
+    public static void main(String[] args) {
+        int a=4;
+        int b=5;
+        for (int i = 1; i <=a; i++) {
+            for (int j = 1; j <=b; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
