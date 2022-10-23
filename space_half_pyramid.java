b07dde4b7ccb4a077b3375940f64c0168cc37133
pkishu <89184771+pkishu@users.noreply.github.com>
2022-10-23 19:44:53 +0530

    Update space_half_pyramid.java

--- space_half_pyramid.java
+++ space_half_pyramid.java
@@ -1,6 +1,6 @@
 public class space_half_pyramid {
     public static void main(String[] args) {
-        int a= 4;
+        int a = 4;
         for (int i = 1; i <= a; i++) {
             for (int j = 1; j <= a-i; j++) {
                 System.out.print(" "); 
