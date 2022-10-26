73e2963e481c09efb36936335dff8563ad9af7bf
Priyanshu Patel <85025907+Kylix-p@users.noreply.github.com>
2022-10-26 22:07:49 +0530

    Update even_numbers.java

--- even_numbers.java
+++ even_numbers.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class even_numbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
+        System.out.print("Enter the number : ");
         int a = sc.nextInt();
 
         for(int i = 1; i<=a; i++){
