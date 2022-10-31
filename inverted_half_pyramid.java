89c59207a6c3040e37a191a135844c00cc31b06c
pkishu <89184771+pkishu@users.noreply.github.com>
2022-10-31 23:23:00 +0530

    Update inverted_half_pyramid.java

--- inverted_half_pyramid.java
+++ inverted_half_pyramid.java
@@ -1,7 +1,7 @@
 public class inverted_half_pyramid {
     public static void main(String[] args) {
         int a=4;
-        for (int i = a; i>=1; i--) {
+        for (int i = a; i >= 1; i--) {
             for (int j = 1; j<=i; j++) {
                 System.out.print("*");
             }
