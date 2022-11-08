ff78a59594d6f92974cef5a1f0a0d2e3f6f95d7e
pkishu <89184771+pkishu@users.noreply.github.com>
2022-11-08 23:41:50 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
+        System.out.print("Enter first number: ");
         int a = sc.nextInt();
 
-        System.out.print("Enter the second number: ");
+        System.out.print("Enter second number: ");
         int b = sc.nextInt();
 
         System.out.print("Select operation: ");
