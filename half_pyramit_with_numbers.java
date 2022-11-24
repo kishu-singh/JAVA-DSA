9595b1a7d1697bb4fda5c21831b62e54e6210b8c
pkishu <89184771+pkishu@users.noreply.github.com>
2022-11-24 23:04:41 +0530

    Update half_pyramit_with_numbers.java

--- half_pyramit_with_numbers.java
+++ half_pyramit_with_numbers.java
@@ -1,6 +1,6 @@
 public class half_pyramit_with_numbers {
     public static void main(String[] args) {
-        int a=5;
+        int a = 5;
         for (int i = 1; i <= a; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
